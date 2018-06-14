82cc1c06876ec4356062ffb1a2b77c631c19ada8
Jess <jesserjohnson@gmail.com>
2018-06-13 21:47:09 -0700

    Solve exercise

--- AnonymousIncomeComparisonTool/Program.cs
+++ AnonymousIncomeComparisonTool/Program.cs
@@ -10,7 +10,19 @@ namespace AnonymousIncomeComparisonTool
     {
         public static void Main()
         {
-
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("Person 1");
+            Console.WriteLine("Hourly Rate?");
+            var answer1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hours worked per week?");
+            var answer2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Person 2");
+            Console.WriteLine("Hourly Rate?");
+            var answer3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hours worked per week?");
+            var answer4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            Console.WriteLine((answer1 * answer2) > (answer3 * answer4));
         }
     }
 }
